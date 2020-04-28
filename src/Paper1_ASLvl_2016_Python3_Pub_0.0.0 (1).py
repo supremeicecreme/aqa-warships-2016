3c0d3b75e1f08b9f734bd868b0e758201444300c
supremeicecreme <thebullasjr@gmail.com>
2020-04-28 09:38:42 +0100

    Refactor to match PEP 8 guidelines

--- src/Paper1_ASLvl_2016_Python3_Pub_0.0.0 (1).py
+++ src/Paper1_ASLvl_2016_Python3_Pub_0.0.0 (1).py
@@ -1,149 +1,162 @@
-#Skeleton Program for the AQA AS1 Summer 2016 examination
-#this code should be used in conjunction with the Preliminary Material
-#written by the AQA AS1 Programmer Team
-#developed in a Python 3 programming environment
+# Skeleton Program for the AQA AS1 Summer 2016 examination
+# this code should be used in conjunction with the Preliminary Material
+# written by the AQA AS1 Programmer Team
+# developed in a Python 3 programming environment
 
-#Version Number 1.0
+# Version Number 1.0
 
 import random
 
-def GetRowColumn():
-  print()
-  Column = int(input("Please enter column: "))
-  Row = int(input("Please enter row: "))
-  print()
-  return Row, Column
-            
-def MakePlayerMove(Board, Ships):
-  Row, Column = GetRowColumn()
-  if Board[Row][Column] == "m" or Board[Row][Column] == "h":
-    print("Sorry, you have already shot at the square (" + str(Column) + "," + str(Row) + "). Please try again.")
-  elif Board[Row][Column] == "-":
-    print("Sorry, (" + str(Column) + "," + str(Row) + ") is a miss.")
-    Board[Row][Column] = "m"
-  else:
-    print("Hit at (" + str(Column) + "," + str(Row) + ").")
-    Board[Row][Column] = "h"
-        
-def SetUpBoard():
-  Board = []
-  for Row in range(10):
-    BoardRow = []
-    for Column in range(10):
-      BoardRow.append("-")
-    Board.append(BoardRow)
-  return Board
 
-def LoadGame(Filename, Board):
-  BoardFile = open(Filename, "r")
-  for Row in range(10):
-    Line = BoardFile.readline()
-    for Column in range(10):
-      Board[Row][Column] = Line[Column]
-  BoardFile.close()
-    
-def PlaceRandomShips(Board, Ships):
-  for Ship in Ships:
-    Valid = False
-    while not Valid:
-      Row = random.randint(0, 9) 
-      Column = random.randint(0, 9) 
-      HorV = random.randint(0, 1)
-      if HorV == 0:
-        Orientation = "v" 
-      else:
-        Orientation = "h" 
-      Valid = ValidateBoatPosition(Board, Ship, Row, Column, Orientation)
-    print("Computer placing the " + Ship[0])
-    PlaceShip(Board, Ship, Row, Column, Orientation)
-
-def PlaceShip(Board, Ship, Row, Column, Orientation):
-  if Orientation == "v":
-    for Scan in range(Ship[1]):
-      Board[Row + Scan][Column] = Ship[0][0]
-  elif Orientation == "h":
-    for Scan in range(Ship[1]):
-      Board[Row][Column + Scan] = Ship[0][0]
-
-def ValidateBoatPosition(Board, Ship, Row, Column, Orientation):
-  if Orientation == "v" and Row + Ship[1] > 10:
-    return False
-  elif Orientation == "h" and Column + Ship[1] > 10:
-    return False
-  else:
-    if Orientation == "v":
-      for Scan in range(Ship[1]):
-        if Board[Row + Scan][Column] != "-":
-          return False
-    elif Orientation == "h":
-      for Scan in range(Ship[1]):
-        if Board[Row][Column + Scan] != "-":
-          return False
-  return True
-
-def CheckWin(Board):
-  for Row in range(10):
-    for Column in range(10):
-      if Board[Row][Column] in ["A","B","S","D","P"]:
-        return False
-  return True
- 
-def PrintBoard(Board):
-  print()
-  print("The board looks like this: ")  
-  print()
-  print (" ", end="")
-  for Column in range(10):
-    print(" " + str(Column) + "  ", end="")
-  print()
-  for Row in range(10):
-    print (str(Row) + " ", end="")
-    for Column in range(10):
-      if Board[Row][Column] == "-":
-        print(" ", end="")
-      elif Board[Row][Column] in ["A","B","S","D","P"]:
-        print(" ", end="")                
-      else:
-        print(Board[Row][Column], end="")
-      if Column != 9:
-        print(" | ", end="")
+def get_row_column():
     print()
-       
-def DisplayMenu():
-  print("MAIN MENU")
-  print()
-  print("1. Start new game")
-  print("2. Load training game")
-  print("9. Quit")
-  print()
-    
-def GetMainMenuChoice():
-  print("Please enter your choice: ", end="")
-  Choice = int(input())
-  print()
-  return Choice
+    column = int(input("Please enter column: "))
+    row = int(input("Please enter row: "))
+    print()
+    return row, column
 
-def PlayGame(Board, Ships):
-  GameWon = False
-  while not GameWon:
-    PrintBoard(Board)
-    MakePlayerMove(Board, Ships)
-    GameWon = CheckWin(Board)
-    if GameWon:
-      print("All ships sunk!")
-      print()
+
+def make_player_move(board, ships):
+    row, column = get_row_column()
+    if board[row][column] == "m" or board[row][column] == "h":
+        print("Sorry, you have already shot at the square (" + str(column) + "," + str(row) + "). Please try again.")
+    elif board[row][column] == "-":
+        print("Sorry, (" + str(column) + "," + str(row) + ") is a miss.")
+        board[row][column] = "m"
+    else:
+        print("Hit at (" + str(column) + "," + str(row) + ").")
+        board[row][column] = "h"
+
+
+def set_up_board():
+    board = []
+    for Row in range(10):
+        board_row = []
+        for Column in range(10):
+            board_row.append("-")
+        board.append(board_row)
+    return board
+
+
+def load_game(filename, board):
+    board_file = open(filename, "r")
+    for Row in range(10):
+        line = board_file.readline()
+        for Column in range(10):
+            board[Row][Column] = line[Column]
+    board_file.close()
+
+
+def place_random_ships(board, ships):
+    for Ship in ships:
+        valid = False
+        while not valid:
+            row = random.randint(0, 9)
+            column = random.randint(0, 9)
+            h_or_v = random.randint(0, 1)
+            if h_or_v == 0:
+                orientation = "v"
+            else:
+                orientation = "h"
+            valid = validate_boat_position(board, Ship, row, column, orientation)
+        print("Computer placing the " + Ship[0])
+        place_ship(board, Ship, row, column, orientation)
+
+
+def place_ship(board, ship, row, column, orientation):
+    if orientation == "v":
+        for Scan in range(ship[1]):
+            board[row + Scan][column] = ship[0][0]
+    elif orientation == "h":
+        for Scan in range(ship[1]):
+            board[row][column + Scan] = ship[0][0]
+
+
+def validate_boat_position(board, ship, row, column, orientation):
+    if orientation == "v" and row + ship[1] > 10:
+        return False
+    elif orientation == "h" and column + ship[1] > 10:
+        return False
+    else:
+        if orientation == "v":
+            for Scan in range(ship[1]):
+                if board[row + Scan][column] != "-":
+                    return False
+        elif orientation == "h":
+            for Scan in range(ship[1]):
+                if board[row][column + Scan] != "-":
+                    return False
+    return True
+
+
+def check_win(board):
+    for Row in range(10):
+        for Column in range(10):
+            if board[Row][Column] in ["A", "B", "S", "D", "P"]:
+                return False
+    return True
+
+
+def print_board(board):
+    print()
+    print("The board looks like this: ")
+    print()
+    print(" ", end="")
+    for Column in range(10):
+        print(" " + str(Column) + "  ", end="")
+    print()
+    for Row in range(10):
+        print(str(Row) + " ", end="")
+        for Column in range(10):
+            if board[Row][Column] == "-":
+                print(" ", end="")
+            elif board[Row][Column] in ["A", "B", "S", "D", "P"]:
+                print(" ", end="")
+            else:
+                print(board[Row][Column], end="")
+            if Column != 9:
+                print(" | ", end="")
+        print()
+
+
+def display_menu():
+    print("MAIN MENU")
+    print()
+    print("1. Start new game")
+    print("2. Load training game")
+    print("9. Quit")
+    print()
+
+
+def get_main_menu_choice():
+    print("Please enter your choice: ", end="")
+    choice = int(input())
+    print()
+    return choice
+
+
+def play_game(board, ships):
+    game_won = False
+    while not game_won:
+        print_board(board)
+        make_player_move(board, ships)
+        game_won = check_win(board)
+        if game_won:
+            print("All ships sunk!")
+            print()
+
 
 if __name__ == "__main__":
-  TRAININGGAME = "Training.txt"
-  MenuOption = 0
-  while not MenuOption == 9:
-    Board = SetUpBoard()
-    Ships = [["Aircraft Carrier", 5], ["Battleship", 4], ["Submarine", 3], ["Destroyer", 3], ["Patrol Boat", 2]]
-    DisplayMenu()
-    MenuOption = GetMainMenuChoice()
-    if MenuOption == 1:
-      PlaceRandomShips(Board, Ships)
-      PlayGame(Board,Ships)
-    if MenuOption == 2:
-      LoadGame(TRAININGGAME, Board)
-      PlayGame(Board, Ships)   
+    training_game = "Training.txt"
+    MenuOption = 0
+    while not MenuOption == 9:
+        Board = set_up_board()
+        Ships = [["Aircraft Carrier", 5], ["Battleship", 4], ["Submarine", 3], ["Destroyer", 3], ["Patrol Boat", 2]]
+        display_menu()
+        MenuOption = get_main_menu_choice()
+        if MenuOption == 1:
+            place_random_ships(Board, Ships)
+            play_game(Board, Ships)
+        if MenuOption == 2:
+            load_game(training_game, Board)
+            play_game(Board, Ships)
